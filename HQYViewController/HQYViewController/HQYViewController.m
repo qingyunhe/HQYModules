545c94bd3d8ef20cde202dbbc3514fb60935a5ba
qingyunhe <heqingyun1989@163.com>
2016-08-10 23:57:10 +0800

    修改

---
+++ HQYViewController/HQYViewController/HQYViewController.m
@@ -0,0 +1,64 @@
+//
+//  HQYViewController.m
+//  HQYViewController
+//
+//  Created by 江城程序猿 on 16/8/9.
+//  Copyright © 2016年 江城程序猿. All rights reserved.
+//
+
+#import "HQYViewController.h"
+
+@interface HQYViewController ()
+/** 自定义选项卡 */
+@property (nonatomic,weak) UIView *tabBarView;
+
+@end
+
+@implementation HQYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UIView *tarBarView = [[UIView alloc] init];
+    self.tabBarView = tarBarView;
+    CGFloat tabBarViewX = 0;
+    CGFloat tabBarViewY = [UIScreen mainScreen].bounds.size.height - 49;
+    CGFloat tabBarViewW = [UIScreen mainScreen].bounds.size.width;
+    CGFloat tabBarViewH = 49;
+    
+    self.tabBarView.frame = CGRectMake(tabBarViewX, tabBarViewY, tabBarViewW, tabBarViewH);
+    self.tabBarView.backgroundColor = [UIColor whiteColor];
+
+    [self.view addSubview:self.tabBarView];
+    
+    
+}
+
+#pragma - mark 添加选项卡中的item
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [self setUpTabBar];
+
+}
+
+- (void)setUpTabBar{
+
+    CGFloat count = self.childViewControllers.count;
+    CGFloat tabBarButtonX = [UIScreen mainScreen].bounds.size.width / count;
+    CGFloat tabBarButtonY = 0;
+    CGFloat tabBarButtonW = [UIScreen mainScreen].bounds.size.width / count;
+    CGFloat tabBarButtonH = 49;
+    for(NSInteger i = 0; i < count; ++i) {
+        
+        UIButton *tabBarButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        tabBarButton.frame = CGRectMake(tabBarButtonX, tabBarButtonY, i * tabBarButtonW, tabBarButtonH);
+        UIViewController *vc = self.childViewControllers[i];
+        [tabBarButton setTitle:vc.title forState:UIControlStateNormal];
+        tabBarButton.backgroundColor = [UIColor orangeColor];
+        [self.tabBarView addSubview:tabBarButton];
+    }
+
+}
+
+
+@end
