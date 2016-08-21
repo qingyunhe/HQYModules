5067f3e6d4c98cfa528f22acdd4d39e5ff0f1e3a
qingyunhe <heqingyun1989@163.com>
2016-08-22 07:48:32 +0800

    update README

--- HQYModules/HQYModules/AppDelegate.m
+++ HQYModules/HQYModules/AppDelegate.m
@@ -16,6 +16,7 @@
 @implementation AppDelegate
 
 #pragma - mark app加载完成
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
